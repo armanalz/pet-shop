8c5ec36a834f5e7c1049b93b78779d877150bf40
Arman Alizadeh <armanalizadeh467@gmail.com>
2020-05-24 23:24:37 +0200

    Updates

---
+++ precache-manifest.6c66581fbd9b90d7e13d091fc44186e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eefb25a9947a03d01b3d6e922c0d6c4",
+    "url": "/pet-shop/index.html"
+  },
+  {
+    "revision": "ff4f0e2c43d9460ba200",
+    "url": "/pet-shop/static/css/main.1f19af96.chunk.css"
+  },
+  {
+    "revision": "3292d12a19212d48104f",
+    "url": "/pet-shop/static/js/2.f2ccd324.chunk.js"
+  },
+  {
+    "revision": "f657a147b3d337b4b54e3f16b70f8006",
+    "url": "/pet-shop/static/js/2.f2ccd324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff4f0e2c43d9460ba200",
+    "url": "/pet-shop/static/js/main.fa8cb48a.chunk.js"
+  },
+  {
+    "revision": "49ed933d3035ce7ab76c",
+    "url": "/pet-shop/static/js/runtime-main.e2dfc538.js"
+  },
+  {
+    "revision": "4fab045646b60a960327327fa6a0dfc0",
+    "url": "/pet-shop/static/media/img-0.4fab0456.jpg"
+  },
+  {
+    "revision": "699ce926c570ed240d8647a80fade517",
+    "url": "/pet-shop/static/media/img-1.699ce926.jpg"
+  },
+  {
+    "revision": "a3cdcd26019f2bc4cf3f2b7a2ce25437",
+    "url": "/pet-shop/static/media/img-2.a3cdcd26.jpg"
+  },
+  {
+    "revision": "38ac1a55f059e318c8028ff27eba4a07",
+    "url": "/pet-shop/static/media/img-3.38ac1a55.jpg"
+  },
+  {
+    "revision": "48d68d381172e89cb41cc58c109ef3eb",
+    "url": "/pet-shop/static/media/slide-0.48d68d38.jpg"
+  },
+  {
+    "revision": "65c1ca316a71a336c2897090a23d69ab",
+    "url": "/pet-shop/static/media/slide-1.65c1ca31.jpg"
+  },
+  {
+    "revision": "b05061fca56795684586af9a6546dc49",
+    "url": "/pet-shop/static/media/slide-2.b05061fc.jpg"
+  },
+  {
+    "revision": "c35f80ef7626765c733bc89e7ec65d80",
+    "url": "/pet-shop/static/media/slide-3.c35f80ef.jpg"
+  },
+  {
+    "revision": "1b530fd3ab79775a1fa0ae4871776a43",
+    "url": "/pet-shop/static/media/sprite.1b530fd3.svg"
+  }
+]);
